5baad6b7c4248f938e8589321dd5a0a59951d481
Ratandeep Arunkumar <ratandeeparun@gmail.com>
2020-06-13 18:27:27 +0530

    Add MongoDB database for backend

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname+"/date.js");
+const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 
 const app = express();
-
-let items = [];
-let workItems = [];
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -14,43 +12,93 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//Creating default items documents
+
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = new mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to todo list!"
+});
+
+const item2 = new Item({
+  name: "Press + button to add new item to your list"
+});
+
+const item3 = new Item({
+  name: "<-- hit this to cross off an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+// Home page render
+
 app.get("/", function(req, res) {
 
-let day = date();
+  const day = date();
 
-  res.render("lists", {
-    listTitle: day,
-    newListItems: items
+  Item.find(function(error, tutorialItems) {
+    if (tutorialItems.length === 0) {
+      Item.insertMany(defaultItems,function(err){
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log("Successfully added default Items");
+        }
+      });
+      res.redirect("/");
+    }else{
+      res.render("lists", {
+        listTitle: day,
+        newListItems: tutorialItems
+      });
+    }
   });
 });
 
-app.get("/work", function(req, res) {
-  res.render("lists", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
-});
+//Work page render
 
-app.get("/about", function(req, res) {
-  res.render("about");
-});
+// app.get("/work", function(req, res) {
+//   res.render("lists", {
+//     listTitle: "Work List",
+//     newListItems: workItems
+//   });
+// });
+//
+// app.get("/about", function(req, res) {
+//   res.render("about");
+// });
+
+
+//Adding item to list
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item)
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+
 });
 
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
-})
+// app.post("/work", function(req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+// })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
